a8a532c3ddc951c28f2fc48f3a9284e54e3c4c44
aggigie <aggigie46@gmail.com>
2020-03-30 23:54:46 +0200

    zad1 - test

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # laboratorium-7-aggigie
 ![Java CI with Gradle](https://github.com/testowanieaplikacjijavaug/laboratorium-7-aggigie/workflows/Java%20CI%20with%20Gradle/badge.svg)
 [![codecov](https://codecov.io/gh/testowanieaplikacjijavaug/laboratorium-7-aggigie/branch/master/graph/badge.svg)](https://codecov.io/gh/testowanieaplikacjijavaug/laboratorium-7-aggigie)
+
 laboratorium-7-aggigie created by GitHub Classroom
---
+++ src/test/java/NoteTest.java
@@ -0,0 +1,62 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class NoteTest {
+
+    Note note;
+
+    @Before
+    public void setUP(){
+        note =  Note.of("Marcin", 4.5f);
+    }
+
+    @Test
+    public void testGetName(){
+        assertThat( note.getName()).isEqualTo("Marcin");
+    }
+
+    @Test
+    public void testGetNote(){
+        assertThat(note.getNote()).isBetween(2.0f,6.0f).isEqualTo(4.5f);
+    }
+
+    @Test
+    public void testNullAsAName(){
+        assertThatThrownBy(() -> {
+            Note newNone = Note.of (null, 3);
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage("Imię ucznia nie może być null");
+    }
+
+    @Test
+    public void testEmptyName(){
+        assertThatThrownBy(() -> {
+            Note newNone = Note.of ("", 3);
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage("Imię ucznia nie może być puste");
+    }
+
+    @Test
+    public void testUncommonName(){
+        assertThat(() -> {
+            Note newNone = Note.of ("Jaga2342", 3.5f);
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage("Imię ucznia nie może być puste");
+    }
+
+    @Test
+    public void testNoteUnder(){
+        assertThatThrownBy(() -> {
+            Note newNone = Note.of ("Marcin", 0.5f);
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage("Niewłaściwa ocena");
+    }
+
+    @Test
+    public void testNoteAbove(){
+        assertThatThrownBy(() -> {
+            Note newNone = Note.of ("Marcin", 8);
+        }).isInstanceOf(IllegalArgumentException.class).hasMessage("Niewłaściwa ocena");
+    }
+}
